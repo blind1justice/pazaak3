3b8cd5b6d577a359936f268fb79a94e57e4dd5c8
Ilya Rabets <ilyarabets@gmail.com>
2025-11-15 17:56:24 +0300

    Add blank other pages

---
+++ client/src/app/pages/connect-to-game-page/connect-to-game-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-connect-to-game-page',
+  imports: [],
+  templateUrl: './connect-to-game-page.html',
+  styleUrl: './connect-to-game-page.scss',
+})
+export class ConnectToGamePage {
+
+}
